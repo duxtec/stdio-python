11c6ae97074c758ddec29b374c1564fe010a7e8c
Dux Tecnologia <dux.tec.br@gmail.com>
2024-07-31 13:59:50 -0300

    bump: update version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stdio',
-    version='1.0.0',
+    version='1.0.1',
     description='Facilitating functions related to reading and writing data on the terminal.',
     author='Dux Tecnologia',
     author_email='contato@dux.tec.br',
